12e9d6f33bd50f764896bb6e83655f5626b793e6
IdkwhatImD0ing <91146598+IdkwhatImD0ing@users.noreply.github.com>
2021-11-17 01:36:03 -0800

    messing around with tensors

--- tensors.py
+++ tensors.py
@@ -5,6 +5,7 @@ import numpy as np
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using {device} device")
 
+
 data = [[1, 2],[3, 4]]
 x_data = torch.tensor(data)
 
@@ -19,3 +20,53 @@ zeros_tensor = torch.zeros(shape)
 print(f"Random Tensor: \n {rand_tensor} \n")
 print(f"Ones Tensor: \n {ones_tensor} \n")
 print(f"Zeros Tensor: \n {zeros_tensor}")
+
+print(f"Shape of tensor: {rand_tensor.shape}")
+print(f"Datatype of tensor: {rand_tensor.dtype}")
+print(f"Device tensor is stored on: {rand_tensor.device}")
+
+# We move our tensor to the GPU if available
+if torch.cuda.is_available():
+    rand_tensor = rand_tensor.to('cuda')
+
+print(f"Device tensor is stored on: {rand_tensor.device}")
+
+tensor = torch.ones(4, 4) #Numpy operations
+print('First row: ', tensor[0])
+print('First column: ', tensor[:, 0])
+print('Last column:', tensor[..., -1])
+tensor[:,1] = 0
+print(tensor)
+print("\n")
+
+t1 = torch.cat([tensor, tensor, tensor], dim=1)
+print(t1)
+
+# This computes the matrix multiplication between two tensors. y1, y2, y3 will have the same value
+y1 = tensor @ tensor.T
+y2 = tensor.matmul(tensor.T)
+
+y3 = torch.rand_like(tensor)
+torch.matmul(tensor, tensor.T, out=y3)
+
+
+# This computes the element-wise product. z1, z2, z3 will have the same value
+z1 = tensor * tensor
+z2 = tensor.mul(tensor)
+
+z3 = torch.rand_like(tensor)
+torch.mul(tensor, tensor, out=z3)
+print("\nY3 tensor (matrix multiplication):\n")
+print(y3)
+print("\nZ3 tensor (regular multiplication):\n")
+print(z3)
+print("\n")
+
+agg = tensor.sum()
+agg_item = agg.item()
+print(agg_item, type(agg_item))
+
+print(tensor, "\n")
+tensor.add_(5)
+print("After adding 5 \n")
+print(tensor)
