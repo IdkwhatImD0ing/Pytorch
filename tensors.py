aff96e29acbb7048fb288ceed8c1ccb1593a6087
IdkwhatImD0ing <91146598+IdkwhatImD0ing@users.noreply.github.com>
2021-11-17 01:19:42 -0800

    Tensors

---
+++ tensors.py
@@ -0,0 +1,21 @@
+import torch
+import numpy as np
+
+# Get cpu or gpu device for training.
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Using {device} device")
+
+data = [[1, 2],[3, 4]]
+x_data = torch.tensor(data)
+
+np_array = np.array(data)
+x_np = torch.from_numpy(np_array)
+
+shape = (2,3,)
+rand_tensor = torch.rand(shape)
+ones_tensor = torch.ones(shape)
+zeros_tensor = torch.zeros(shape)
+
+print(f"Random Tensor: \n {rand_tensor} \n")
+print(f"Ones Tensor: \n {ones_tensor} \n")
+print(f"Zeros Tensor: \n {zeros_tensor}")
